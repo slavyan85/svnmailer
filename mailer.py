6cf5c302df4154b50a26ab8eda8cc4b7d52a7f2c
slavyan85 <slavyan85@users.noreply.github.com>
2015-09-08 02:16:29 +0300

    #2. Check authors and process commits them.

--- mailer.py
+++ mailer.py
@@ -2,6 +2,7 @@
 __author__ = 'v.kryuchenko'
 
 
+import argparse
 import pysvn
 import json
 import smtplib
@@ -13,6 +14,7 @@ class Mailer:
     def __init__(self, config):
         with open(config, 'r') as cf:
             options = json.loads(cf.read(), encoding='utf8')
+        self.tracker = options['tracker']
         self.smpt_url = options['sender']['server_url']
         self.smpt_login = options['sender']['login']
         self.smpt_pass = options['sender']['password']
@@ -20,25 +22,49 @@ class Mailer:
         self.depth = int(options['depth'])
         self.developers = options['developers']
         self.watchers = options['watchers']
-        self.today = datetime.datetime.today()
-        self.yesterday = self.today.replace(day=(self.today.day - 1))
+        today = datetime.datetime.today()
+        self.yesterday = today.replace(day=(today.day - 1))
 
-    def get_commits(self):
+    def commits_by_authors(self):
+        """
+        :return: dict(author=dict(revision=message))
+        """
+        result = {}
         svn = pysvn.Client()
         start_revision = svn.log(self.svn_root, limit=1, discover_changed_paths=False)[0].revision.number
         end_revision = start_revision - self.depth
         draft_log = svn.log(self.svn_root,
                             revision_start=pysvn.Revision(pysvn.opt_revision_kind.number, start_revision),
                             revision_end=pysvn.Revision(pysvn.opt_revision_kind.number, end_revision))
-        commits = None
+        log = [commit for commit in draft_log if commit.author in self.developers and datetime.datetime.fromtimestamp(commit.date) > self.yesterday]
+        for commit in log:
+            print()
+            author = commit.author
+            revision = commit.revision.number
+            message = commit.message
+            if author not in result:
+                result.update({author: {revision: message}})
+            else:
+                result[author].update({revision: message})
+        return result
 
     def send_email(self, watcher):
         pass
 
     def run(self):
-        pass
+        authors = self.commits_by_authors()
+        for author in authors:
+            print(author)
 
 
 if __name__ == '__main__':
-    mailer = Mailer('config.json')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config',
+                        action='store',
+                        dest='config_file',
+                        default='config.json',
+                        help='Path to configuration file. Default <config.json>')
+    opts = parser.parse_args()
+
+    mailer = Mailer(opts.config_file)
     mailer.run()
