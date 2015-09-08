8a6bfe8c283b35d01642a351d5d76db783d8c9ba
slavyan85 <slavyan85@users.noreply.github.com>
2015-09-08 03:22:39 +0300

    #3. Add email template

--- mailer.py
+++ mailer.py
@@ -15,15 +15,18 @@ class Mailer:
         with open(config, 'r') as cf:
             options = json.loads(cf.read(), encoding='utf8')
         self.tracker = options['tracker']
-        self.smpt_url = options['sender']['server_url']
-        self.smpt_login = options['sender']['login']
-        self.smpt_pass = options['sender']['password']
+        self.smtp_url = options['sender']['server_url']
+        self.smtp_login = options['sender']['login']
+        self.smtp_pass = options['sender']['password']
+        self.signature = options['signature']
         self.svn_root = options['svn_root']
         self.depth = int(options['depth'])
         self.developers = options['developers']
         self.watchers = options['watchers']
         today = datetime.datetime.today()
         self.yesterday = today.replace(day=(today.day - 1))
+        with open(options['sender']['template'], 'r') as tf:
+            self.template = options['sender']['template'] = tf.read()
 
     def commits_by_authors(self):
         """
@@ -38,7 +41,6 @@ class Mailer:
                             revision_end=pysvn.Revision(pysvn.opt_revision_kind.number, end_revision))
         log = [commit for commit in draft_log if commit.author in self.developers and datetime.datetime.fromtimestamp(commit.date) > self.yesterday]
         for commit in log:
-            print()
             author = commit.author
             revision = commit.revision.number
             message = commit.message
@@ -48,13 +50,27 @@ class Mailer:
                 result[author].update({revision: message})
         return result
 
-    def send_email(self, watcher):
-        pass
+    def send_email(self, target, message):
+        msg_object = MIMEText(_text=message, _subtype='html', _charset='utf8')
+        msg_object['Subject'] = 'Commits at {}'.format(str(self.yesterday))
+        msg_object['From'] = '{s} <{a}>'.format(s=self.signature, a=self.smtp_login)
+        msg_object['To'] = target
+        try:
+            smtp = smtplib.SMTP_SSL(host=self.smtp_url)
+            smtp.login(self.smtp_login, self.smtp_pass)
+            smtp.sendmail(self.smtp_login, [target], msg_object)
+        except Exception as ex:
+            print('SMTP error.\n{}'.format(ex))
 
     def run(self):
+        content_list = []
         authors = self.commits_by_authors()
         for author in authors:
-            print(author)
+            author_commits = '<br>'.join(['{r}\t:\t{m}'.format(r=commit['revision'], m=commit['message']) for commit in authors[author]])
+            content_list.append(author_commits)
+        content = '<hr>'.join(content_list)
+        print(self.template.format(content=content))
+
 
 
 if __name__ == '__main__':
