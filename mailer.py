e0a523783496aa12fe0336e9f402e1e2d5fa380d
slavyan85 <slavyan85@users.noreply.github.com>
2015-10-01 16:24:20 +0300

    fix bug with date delta

--- mailer.py
+++ mailer.py
@@ -12,6 +12,11 @@ from email.mime.text import MIMEText
 
 
 class Mailer:
+    @staticmethod
+    def __target_date(start_date, delta):
+        result = start_date - datetime.timedelta(days=delta)
+        return result
+
     def __init__(self, config):
         with open(config, 'r') as cf:
             options = json.loads(cf.read(), encoding='utf8')
@@ -21,11 +26,12 @@ class Mailer:
         self.smtp_pass = options['sender']['password']
         self.signature = options['sender']['signature']
         self.svn_root = options['svn_root']
-        self.depth = int(options['depth'])
+        self.commits = int(options['commits'])
+        self.days = int(options['days'])
         self.developers = options['developers']
         self.watchers = options['watchers']
-        self.today = datetime.datetime.today()
-        self.yesterday = self.today.replace(day=(self.today.day - 1))
+        self.stop_day = datetime.datetime.today()
+        self.start_day = self.__target_date(self.stop_day, self.days)
         with open(options['sender']['template'], 'r') as tf:
             self.template = options['sender']['template'] = tf.read()
 
@@ -50,11 +56,11 @@ class Mailer:
         if self.svn_root.startswith('https'):
             svn.callback_ssl_server_trust_prompt = ssl_server_trust_prompt
         start_revision = svn.log(self.svn_root, limit=1, discover_changed_paths=False)[0].revision.number
-        end_revision = start_revision - self.depth
+        end_revision = start_revision - self.commits
         draft_log = svn.log(self.svn_root,
                             revision_start=pysvn.Revision(pysvn.opt_revision_kind.number, start_revision),
                             revision_end=pysvn.Revision(pysvn.opt_revision_kind.number, end_revision))
-        log = [commit for commit in draft_log if commit.author in self.developers and datetime.datetime.fromtimestamp(commit.date) > self.yesterday]
+        log = [commit for commit in draft_log if commit.author in self.developers and datetime.datetime.fromtimestamp(commit.date) > self.start_day]
         for commit in log:
             author = commit.author
             revision = str(commit.revision.number)
@@ -67,8 +73,8 @@ class Mailer:
 
     def send_email(self, target, message):
         msg_object = MIMEText(_text=message, _subtype='html', _charset='utf8')
-        start_time = self.yesterday.strftime('%d-%m-%y %H:%M')
-        end_time = self.today.strftime('%d-%m-%Y %H:%M')
+        start_time = self.start_day.strftime('%d-%m-%y %H:%M')
+        end_time = self.stop_day.strftime('%d-%m-%Y %H:%M')
         msg_object['Subject'] = 'Commits between {s} -- {e}'.format(s=start_time, e=end_time)
         msg_object['From'] = '{s} <{a}>'.format(s=self.signature, a=self.smtp_login)
         msg_object['To'] = target
@@ -84,7 +90,7 @@ class Mailer:
     def render_template(self, authors, watcher):
         content_list = []
         for author in authors:
-            author_commits = '{}<br>'.format(author) if author in self.watchers[watcher] else ''
+            author_commits = '<b>{}</b><br>'.format(author) if author in self.watchers[watcher] else ''
             author_commits += '<br>'.join(['{r}\t:\t{m}'.format(r=commit, m=authors[author][commit]) for commit in authors[author] if author in self.watchers[watcher]])
             content_list.append(author_commits)
         content = '<hr>'.join([content_item for content_item in content_list if content_item])
